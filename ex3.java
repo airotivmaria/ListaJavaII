2f92849f0dd0f2a781e4c58f8a0973ad1a5a2133
airotivmaria <mariavitoria6019@gmail.com>
2024-09-30 11:59:12 -0300

    fazendo - incompleto

---
+++ ex3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] numeros = new int[5][10];
+
+        for(int i = 0; i < 5; i++){
+            for (int c = 0; c < 10; c++) {
+                System.out.println("Digite um valor para a coluna " + c + " da linha " + i);
+                numeros[][c] = 
+            }
+        }
+    }
+}
